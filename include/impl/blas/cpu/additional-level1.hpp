1ac1c5bfa900c770103801cb6d9cac257bf4777d
Pit Steinbach <steinbach@pc.rwth-aachen.de>
2025-03-11 09:41:30 +0100

    add doc remove not implmented features

--- include/impl/blas/cpu/additional-level1.hpp
+++ include/impl/blas/cpu/additional-level1.hpp
@@ -4,72 +4,144 @@
 #include "const.h"
 #include "level1.h"
 
-namespace lahva{
+namespace lahva
+{
     namespace cpu
     {
-        
-        template<typename T>
-        double ComputeTrace(const CPURuntime& cudart, const Matrix_<T>&, bool use_diag = false);
-        double ComputeTrace(const CPURuntime& cudart, const Vector_<double>& diag);
+        /// @brief Compute the trace of a matrix
+        /// @tparam T Numerical type of the matrix
+        /// @param cpurt CPU Runtime instance
+        /// @param matrix Matrix to compute the trace of
+        /// @param use_diag Use the diagonal of the matrix
+        /// @return The trace of the matrix
+        template <typename T>
+        double ComputeTrace(const CPURuntime &cpurt, const Matrix_<T> &, bool use_diag = false);
 
-        template<typename T>
-        T FrobeniusNorm(const Tensor<T>& mat);
+        /// @brief Compute the trace of a matrix
+        /// @param cpurt CPU Runtime instance
+        /// @param diag Vector containing the diagonal of the matrix
+        /// @return The trace of the matrix
+        double ComputeTrace(const CPURuntime &cpurt, const Vector_<double> &diag);
 
-        template<typename T>
-        T FrobeniusNorm(const Tensor<T>& mat, const Tensor<T>& mat2);
+        /// @brief Compute the Frobenius norm of a matrix
+        /// @tparam T Numerical type of the matrix
+        /// @param mat Matrix to compute the Frobenius norm of
+        /// @return The Frobenius norm of the matrix
+        template <typename T>
+        T FrobeniusNorm(const Tensor<T> &mat);
 
-        template<typename T>
-        T FrobeniusNorm(const CPURuntime& cudart, const Tensor<T>& mat, const Tensor<T>& mat2)
+        /// @brief Compute the Frobenius norm of the difference of two matrices
+        /// @tparam T Numerical type of the matrix
+        /// @param mat Matrix to compute the Frobenius norm of
+        /// @param mat2 Matrix to compute the Frobenius norm of
+        /// @return The Frobenius norm of the difference of the two matrices
+        template <typename T>
+        T FrobeniusNorm(const Tensor<T> &mat, const Tensor<T> &mat2);
+
+        /// @brief Compute the Frobenius norm of the difference of two matrices
+        /// @tparam T Numerical type of the matrix
+        /// @param cpurt CPU Runtime instance
+        /// @param mat Matrix to compute the Frobenius norm of
+        /// @param mat2 Matrix to compute the Frobenius norm of
+        /// @return The Frobenius norm of the difference of the two matrices
+        template <typename T>
+        T FrobeniusNorm(const CPURuntime &cpurt, const Tensor<T> &mat, const Tensor<T> &mat2)
         {
             return FrobeniusNorm(mat, mat2);
         };
 
-        template<typename T>
-        T FrobeniusNorm(const CPURuntime& cudart, const Tensor<T>& mat);
+        /// @brief Compute the Frobenius norm of a matrices
+        /// @tparam T Numerical type of the matrix
+        /// @param cpurt CPU Runtime instance
+        /// @param mat Matrix to compute the Frobenius norm of
+        /// @return The Frobenius norm of the matrix
+        template <typename T>
+        T FrobeniusNorm(const CPURuntime &cpurt, const Tensor<T> &mat);
 
-        template<typename T>
-        T FrobeniusInnerProduct(const Matrix_<T>& mat1, const Matrix_<T>& mat2);
-        template<typename T>
-        T FrobeniusInnerProduct(const CPURuntime& rt, const Matrix_<T>& mat1, const Matrix_<T>& mat2);
+       
+        template <typename T>
+        T FrobeniusInnerProduct(const Matrix_<T> &mat1, const Matrix_<T> &mat2);
+        template <typename T>
+        T FrobeniusInnerProduct(const CPURuntime &rt, const Matrix_<T> &mat1, const Matrix_<T> &mat2);
 
-        template<typename T, typename U>
-        void GetDiagonal(const CPURuntime& cudart, const Matrix<T, U>& mat, Vector<T,U>& vec)
+        /// @brief Get the diagonal of a matrix
+        /// @tparam T Numerical type of the matrix
+        /// @tparam U Allocator type of the matrix
+        /// @param cpurt CPU Runtime instance
+        /// @param mat Matrix to get the diagonal of
+        /// @param vec Vector to store the diagonal in
+        template <typename T, typename U>
+        void GetDiagonal(const CPURuntime &cpurt, const Matrix<T, U> &mat, Vector<T, U> &vec)
         {
             vec = mat.get_diagonal();
         };
 
-        template<typename T, typename U>
-        void SetDiagonal(const CPURuntime& cudart, const Vector<T, U>& vec, Matrix<T, U>& m)
+        /// @brief Set the diagonal of a matrix
+        /// @tparam T Numerical type of the matrix
+        /// @tparam U Allocator type of the matrix
+        /// @param cpurt CPU Runtime instance
+        /// @param vec Vector containing the diagonal
+        /// @param m Matrix to set the diagonal of
+        template <typename T, typename U>
+        void SetDiagonal(const CPURuntime &cpurt, const Vector<T, U> &vec, Matrix<T, U> &m)
         {
             m.set_diagonal(vec);
-        }; 
+        };
+// We need to have the get and set Routiunes for the CPU data of a GPU Tensor as well
 #ifdef _CUDA
-        template<typename T, typename U, typename V>
-        void GetDiagonal(const CPURuntime& cudart, const gpu::Matrix<T, U, V>& mat, gpu::Vector<T, U, V>& vec)
+
+        /// @brief Get the diagonal of a matrix
+        /// @tparam T Numerical type of the matrix
+        /// @tparam U Allocator type of the matrix
+        /// @tparam V GPU Allocator type of the matrix
+        /// @param cpurt CPU Runtime instance
+        /// @param mat Matrix to get the diagonal of
+        /// @param vec Vector to store the diagonal in
+        template <typename T, typename U, typename V>
+        void GetDiagonal(const CPURuntime &cpurt, const gpu::Matrix<T, U, V> &mat, gpu::Vector<T, U, V> &vec)
         {
             size_t max_dim = std::max(mat.shape().first, mat.shape().second);
-            cpu::CopyVectors(vec.size(), mat->data(), max_dim+1 ,vec.data(), 1);
+            cpu::CopyVectors(vec.size(), mat->data(), max_dim + 1, vec.data(), 1);
         };
-        template<typename T, typename U, typename V>
-        void GetDiagonal(const gpu::Matrix<T, U, V>& mat, gpu::Vector<T, U, V>& vec)
+
+        /// @brief Get the diagonal of a matrix
+        /// @tparam T Numerical type of the matrix
+        /// @tparam U Allocator type of the matrix
+        /// @tparam V GPU Allocator type of the matrix
+        /// @param mat Matrix to get the diagonal from
+        /// @param vec Vector to store the diagonal in
+        template <typename T, typename U, typename V>
+        void GetDiagonal(const gpu::Matrix<T, U, V> &mat, gpu::Vector<T, U, V> &vec)
         {
             size_t max_dim = std::max(mat.shape().first, mat.shape().second);
-            cpu::CopyVectors(vec.size(), mat.data(), max_dim+1 ,vec.data(), 1);
+            cpu::CopyVectors(vec.size(), mat.data(), max_dim + 1, vec.data(), 1);
         };
 
-
-        template<typename T, typename U, typename V>
-        void SetDiagonal(const CPURuntime& cudart, const gpu::Vector<T, U, V>& vec, gpu::Matrix<T, U, V>& m)
+        /// @brief Set the diagonal of a matrix
+        /// @tparam T Numerical type of the matrix
+        /// @tparam U Allocator type of the matrix
+        /// @tparam V GPU Allocator type of the matrix
+        /// @param cpurt CPU Runtime instance
+        /// @param vec Vector containing the diagonal
+        /// @param m Matrix to set the diagonal of
+        template <typename T, typename U, typename V>
+        void SetDiagonal(const CPURuntime &cpurt, const gpu::Vector<T, U, V> &vec, gpu::Matrix<T, U, V> &m)
         {
             m.set_diagonal(vec);
-        };  
+        };
 
 #endif
-        template<typename T>
-        using func_t1D = T (*) (T);
 
-        template<typename T>
-        void ApplyKernel(func_t1D<T> funcPtr , Tensor<T>& mat)
+        /// define the function pointer type for the kernel, with 1 input variable
+        template <typename T>
+        using func_t1D = T (*)(T);
+
+        /// @brief Apply a kernel to a matrix
+        /// @tparam T Numerical type of the matrix
+        /// @param funcPtr Function pointer to the kernel
+        /// @param mat Matrix to apply the kernel to
+        template <typename T>
+        void ApplyKernel(func_t1D<T> funcPtr, Tensor<T> &mat)
         {
             #pragma omp parallel for collapse(1)
             for (size_t i = 0; i < mat.size(); i++)
@@ -78,11 +150,16 @@ namespace lahva{
             }
         };
 
-        template<typename T>
-        using func_t0D = T (*) ();
-
-        template<typename T>
-        void ApplyKernel(func_t0D<T> funcPtr , Tensor<T>& mat)
+        /// define the function pointer type for the kernel, with 0 input variables
+        template <typename T>
+        using func_t0D = T (*)();
+        
+        /// @brief Apply a kernel to a matrix
+        /// @tparam T Numerical type of the matrix
+        /// @param funcPtr Function pointer to the kernel
+        /// @param mat Matrix to apply the kernel to
+        template <typename T>
+        void ApplyKernel(func_t0D<T> funcPtr, Tensor<T> &mat)
         {
             #pragma omp parallel for collapse(1)
             for (size_t i = 0; i < mat.size(); i++)
@@ -91,14 +168,14 @@ namespace lahva{
             }
         };
 
-        template<typename... Args>
-        void ApplyKernel(const CPURuntime& rt_, Args&&... args) {
-            (ApplyKernel(args...));                    
+        // Applay Kernel function with CPU runtime, which is not used
+        template <typename... Args>
+        void ApplyKernel(const CPURuntime &rt_, Args &&...args)
+        {
+            (ApplyKernel(args...));
         }
-        
 
-       
     } // namespace gpu
-    
-}   
+
+}
 #endif
--- include/impl/blas/cpu/additional-level2.hpp
+++ include/impl/blas/cpu/additional-level2.hpp
@@ -3,23 +3,26 @@
 #include "linalg.hpp"
 #include "const.h"
 
-namespace lahva{
+namespace lahva
+{
     namespace cpu
     {
-        
-        template<typename T>
-        void AddMatrices(const char* Ta, const char* Tb, const T alpha, const Matrix_<T>& a, const T beta, 
-        const Matrix_<T>& b, Matrix_<T>& c);
-        template<typename T>
-        void AddMatrices(const Matrix_<T>& a, const Matrix_<T>& b, Matrix_<T>& c,
-        const T alpha = 1.0 , const T beta = 1.0, const char* Ta = "N", const char* Tb = "N");
 
-        template<typename... Args>
-        void AddMatrices(const CPURuntime& rt_, Args&&... args) {
-        (AddMatrices(args...));                
+        
+        template <typename T>
+        void AddMatrices(const char *Ta, const char *Tb, const T alpha, const Matrix_<T> &a, const T beta,
+                         const Matrix_<T> &b, Matrix_<T> &c);
+        template <typename T>
+        void AddMatrices(const Matrix_<T> &a, const Matrix_<T> &b, Matrix_<T> &c,
+                         const T alpha = 1.0, const T beta = 1.0, const char *Ta = "N", const char *Tb = "N");
+
+        template <typename... Args>
+        void AddMatrices(const CPURuntime &rt_, Args &&...args)
+        {
+            (AddMatrices(args...));
         }
 
     } // namespace gpu
-    
-}   
+
+}
 #endif
