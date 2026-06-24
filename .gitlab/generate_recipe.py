0cca11b99c74427eca179ccc7345a7bee392d133
Mark Heezen <heezen@pc.rwth-aachen.de>
2026-06-24 08:43:26 +0200

    Install meson and cmake

--- .gitlab/generate_recipe.py
+++ .gitlab/generate_recipe.py
@@ -114,6 +114,7 @@ def generate_recipe(name: str) -> str:
     lines.append("    dnf -y update")
     lines.append("    dnf groupinstall -y \"Development Tools\"")
     lines.append("    dnf install -y wget git python3-pip")
+    lines.append("    sudo dnf install -y cmake meson ninja-build")
     lines.append("    dnf install -y epel-release")
     lines.append("    dnf config-manager --set-enabled crb")
     lines.append("")
